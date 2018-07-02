013f74200dc9b447ca06f0b51e45bbe5f9c2cb11
Test Server <dakproductions25@gmail.com>
2018-07-02 20:56:56 +0000

    Changes to be committed: 	modified:   db.sqlite3 	modified:   pytube/settings.py

--- pytube/settings.py
+++ pytube/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'pytube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pytube',
+        'USER': 'pytubeuser',
+        'PASSWORD': 'youtubesucks',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
