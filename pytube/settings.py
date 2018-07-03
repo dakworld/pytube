38238d514c5305b246cc5f5a3a78faf3a11b112e
Misha K <mishakmak@gmail.com>
2018-07-03 11:27:30 -0400

    Changes to be committed: 	modified:   pytube/settings.py

--- pytube/settings.py
+++ pytube/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'audit_log.middleware.UserLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'pytube.urls'
