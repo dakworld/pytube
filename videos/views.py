932f12cf86ccb9f094aa693930498fddc82fe975
Misha K <mishakmak@gmail.com>
2018-07-02 20:49:34 -0400

    Changes to be committed: 	modified:   videos/views.py

--- videos/views.py
+++ videos/views.py
@@ -23,7 +23,7 @@ class SearchView(generic.ListView):
 
     def get_queryset(self):
         query = self.request.GET['search']
-        search = Video.objects.annotate(distance=TrigramSimilarity('title', query),).filter(distance__gt=0.2).order_by('-distance')
+        search = Video.objects.annotate(distance=TrigramSimilarity('title', query),).order_by('-distance')
         return search
 
 class VideoView(generic.DetailView):
