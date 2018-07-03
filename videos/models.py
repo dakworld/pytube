868e064bc956d60939a5e4d31ba61458cc5f31df
Misha K <mishakmak@gmail.com>
2018-07-03 11:26:07 -0400

    Changes to be committed: 	modified:   videos/models.py

--- videos/models.py
+++ videos/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Playlist(models.Model):
-    title = models.CharField(max_length=100)
-    uploader = models.CharField(max_length=25)
-    videos = models.ManyToManyField(Video)
-    def __str__(self):
-        return self.title
-
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
@@ -21,6 +14,13 @@ class Video(models.Model):
         return self.uploader + ':' + self.title
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=100)
+    uploader = models.CharField(max_length=25)
+    videos = models.ManyToManyField(Video)
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
