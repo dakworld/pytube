e8397466af3533823e8b2e09eb671fcc207f5d4d
Misha <mishakmak@gmail.com>
2018-07-07 12:31:17 -0400

    Changes to be committed: 	modified:   videos/models.py

--- videos/models.py
+++ videos/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.contrib import postgres
+from django.core import mail
 from django.contrib.auth.models import User
+
+class SubscriptionManager(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    emails = postgres.fields.ArrayField(models.EmailField(max_length=60))
+    template_subject = models.CharField(max_length=50)
+    template_message = models.TextField(max_length=1000)
+    def __str__(self):
+        return self.created_by.username + ':' + self.title
+    def send_mail(self, subject, body):
+        mail.send_mail(subject, body, str(self.created_by.username) + '@pytube.localhost', list(self.emails))
+    def add_video_and_send_email(self, video):
+        kwargs = {
+            'name': self.title,
+            'username': self.created_by.username,
+            'title': video.title, 
+            'description': video.description, 
+            'date': video.pub_date, 
+            'url': '/videos/'+str(video.id),
+        }
+        self.send_mail(self.template_subject.format(**kwargs), self.template_message.format(**kwargs))
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     uploader = models.CharField(max_length=25)
+    subscription_manager = models.ManyToManyField(SubscriptionManager)
     up_votes = models.IntegerField(default=1)
     views = models.IntegerField(default=1)
     video_file = models.FileField(upload_to='uploads/videos/')
@@ -35,7 +59,7 @@ class Subtitle(models.Model):
 class Comment(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    message = models.CharField(max_length=500)
+    message = models.TextField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True, editable=False)
     def __str__(self):
         return self.name + '::' + str(self.pub_date)
